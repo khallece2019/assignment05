cacc85de07cea542bf294d5216d26ecd559b9798
Michael R Mackliet <mackliet@eng.utah.edu>
2016-02-12 14:23:27 -0700

    Added case to call for insertionSort, added AnalysisDocInstructions

--- SortUtil.java
+++ SortUtil.java
@@ -4,29 +4,54 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class SortUtil{
-	private int threshold;
-	
-	public static <T> void insertionSort(T[] data, Comparator<? super T> comparator){
+	private static int threshold;
+	private static int thresholdSize = 16;
+	public static <T> void insertionSort(ArrayList<T> data, Comparator<? super T> comparator){
 
-		for(int i=1; i < data.length; i++){
-			T index = data[i];
+		for(int i=1; i < data.size(); i++){
+			T index = data.get(i);
 			int j = i;
 			
-			while (j > 0 && comparator.compare(data[j - 1], index) > 0) {
-				data[j] = data[j - 1];
+			while (j > 0 && comparator.compare(data.get(j-1), index) > 0) {
+				data.set(j, data.get(j-1));
 				j--;
 			}
-			data[j] = index;
+			data.set(j, index);
 		}
 	}
 	
+	//Driver method
 	public static <T> void mergesort(ArrayList<T> list, Comparator<? super T> comparator){
-		
+		int left = 0;
+		int right = list.size();
+		mergeSortRecursive(list, comparator, left, right);
 	}
 	
-	public static <T> void quicksort(ArrayList<T> list, Comparator<? super T> comparator){
-		
+	private static <T> void mergeSortRecursive(ArrayList<T> list, Comparator<? super T> comparator, int left, int right){
+		if(threshold<thresholdSize){
+			insertionSort(list, comparator);
+		}
+		else{
+			
+		}
 	}
+	
+	//Driver method
+	public static <T> void quicksort(ArrayList<T> list, Comparator<? super T> comparator){
+		int left = 0;
+		int right = list.size();
+		quicksortRecursive(list, comparator, left, right);
+	}
+	
+	private static <T> void quicksortRecursive(ArrayList<T> list, Comparator<? super T> comparator, int left, int right){
+		if(threshold<thresholdSize){
+			insertionSort(list, comparator);
+		}
+		else{
+			
+		}
+	}
+	
 	
 	public static ArrayList<Integer> generateBestCase(int size){
 		return null;
