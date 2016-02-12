b1fb212134ab8384e24d9e1f71907c3d1e80d309
Michael R Mackliet <mackliet@eng.utah.edu>
2016-02-12 13:52:08 -0700

    Added insertionSort

--- SortUtil.java
+++ SortUtil.java
@@ -4,9 +4,24 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class SortUtil{
+	private int threshold;
+	
+	public static <T> void insertionSort(T[] data, Comparator<? super T> comparator){
 
+		for(int i=1; i < data.length; i++){
+			T index = data[i];
+			int j = i;
+			
+			while (j > 0 && comparator.compare(data[j - 1], index) > 0) {
+				data[j] = data[j - 1];
+				j--;
+			}
+			data[j] = index;
+		}
+	}
+	
 	public static <T> void mergesort(ArrayList<T> list, Comparator<? super T> comparator){
-
+		
 	}
 	
 	public static <T> void quicksort(ArrayList<T> list, Comparator<? super T> comparator){
