0736d4ae756e043239d270c1656d0a38331af09a
Michael R Mackliet <mackliet@eng.utah.edu>
2016-02-12 13:44:06 -0700

    Added SortUtil class and unimplemented methods

---
+++ SortUtil.java
@@ -0,0 +1,27 @@
+package assignment05;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class SortUtil{
+
+	public static <T> void mergesort(ArrayList<T> list, Comparator<? super T> comparator){
+
+	}
+	
+	public static <T> void quicksort(ArrayList<T> list, Comparator<? super T> comparator){
+		
+	}
+	
+	public static ArrayList<Integer> generateBestCase(int size){
+		return null;
+	}
+	
+	public static ArrayList<Integer> generateAverageCase(int size){
+		return null;
+	}
+	
+	public static ArrayList<Integer> generateWorstCase(int size){
+		return null;
+	}
+}
